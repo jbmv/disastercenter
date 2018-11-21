c4693f52cf29cc1a3895c2b0a1bd7c23c53c401b
james <james-bullard@uiowa.edu>
2018-11-20 22:57:34 -0600

    implement create new request skeleton pages

---
+++ WebContent/WEB-INF/jsp/createNewRequest.jsp
@@ -0,0 +1,92 @@
+<%-- 
+    Document   : newResponse
+    Created on : Nov 19, 2018, 12:05:29 AM
+    Author     : james
+--%>
+
+<%@page import="DisasterCenter.Product"%>
+<%@page import="java.util.Map"%>
+<%@page import="java.util.Iterator"%>
+<%@page import="DisasterCenter.ProductList"%>
+<%@page import="DisasterCenter.Request"%>
+<%@page import="DisasterCenter.Response"%>
+<%@page import="DisasterCenter.RequestList"%>
+<%@page import="DisasterCenter.Location"%>
+<%@page import="DisasterCenter.User"%>
+<%
+    //import java objects from HTTP session
+    ProductList productList = (ProductList) session.getAttribute("productList");
+
+%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <title>Disaster Center</title>
+    <style>
+        input[type=text], select {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        input[type=submit] {
+            width: 100%;
+            background-color: #4CAF50;
+            color: white;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type=submit]:hover {
+            background-color: #45a049;
+        }
+
+    </style>
+    <body>
+        <!-- Sidebar -->
+        <jsp:include page="sidebar.jsp"></jsp:include>
+
+            <!-- Page Content -->
+            <div style="margin-left:15%">
+                <div class="w3-container w3-teal">
+                    <h1>Create A New Request</h1>
+                </div>
+
+                <div style="padding: 20px">
+                    <form action="confirmNewRequest" method="POST">
+
+
+                        <label for="productID">Product Donating</label>
+                        <select id="productID" name="productID">
+                        <%                    // for every entry in requestList.instances, create one table row
+                            Iterator it = productList.getInstances().entrySet().iterator();
+                            while (it.hasNext()) {
+                                Map.Entry pair = (Map.Entry) it.next();
+                                Product newProduct = (Product) productList.getInstances().get(pair.getKey());
+                        %>
+                        <option value="<%= newProduct.getProdId()%>"><% out.print(newProduct.getProdType()); %></option>
+                        <% }%>
+                    </select>
+
+
+                    <label for="quantity">Quantity Donating</label>
+                    <input type="text" id="quantity" name="quantity" placeholder="Enter Quantity">
+
+
+
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ src/Servlets/newRequest.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import DisasterCenter.Product;
+import DisasterCenter.ProductList;
+import DisasterCenter.Queries;
+
+/**
+ *
+ * @author james
+ */
+public class newRequest extends HttpServlet {
+
+	/**
+	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+	 * methods.
+	 *
+	 * @param request  servlet request
+	 * @param response servlet response
+	 * @throws ServletException       if a servlet-specific error occurs
+	 * @throws IOException            if an I/O error occurs
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, ClassNotFoundException, SQLException {
+		response.setContentType("text/html;charset=UTF-8");
+
+		// REQURED TO BE LOGGED IN TO ACCESS THIS PAGE, if not logged in, redirect
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			// user not logged in, forward to login page
+			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/login.html");
+			dispatcher.forward(request, response);
+		}
+
+		try (PrintWriter out = response.getWriter()) {
+
+			// TODO code this servlet
+
+			// get all products from database, append to product list
+			// this is so we can populate the product dropdown when making a custom request
+			// NOTE: we should switch this to stored proceedure
+			Class.forName("com.mysql.jdbc.Driver"); // MySQL database connection
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Project?&useSSL=false", "root",
+					"password");
+			PreparedStatement pst = conn.prepareStatement(Queries.getProducts);
+			ResultSet rs = pst.executeQuery();
+
+			// create ProductList object to append all products to
+			ProductList productList = new ProductList();
+
+			while (rs.next()) {
+				// iterate through all rows in product table, append to productList
+				Product newProduct = new Product(rs.getInt("productID"));
+				newProduct.setProductType(rs.getString("type"));
+
+				// append each request to requestList object
+				productList.addInstance(newProduct);
+			}
+
+			// append productList to HTTP session for use in the view,
+			// then forward browser to createDonation.jsp view
+			session.setAttribute("productList", productList);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/createNewRequest.jsp");
+			dispatcher.forward(request, response);
+
+		}
+	}
+
+	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
+	// + sign on the left to edit the code.">
+	/**
+	 * Handles the HTTP <code>GET</code> method.
+	 *
+	 * @param request  servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException      if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			processRequest(request, response);
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Handles the HTTP <code>POST</code> method.
+	 *
+	 * @param request  servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException      if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			processRequest(request, response);
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Returns a short description of the servlet.
+	 *
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
+
+}
